bc6a4c5925ef8a3d229f9ec656a0b68945eee223
Circle1t <circle1t@outlook.com>
2025-02-14 20:05:18 +0800

    修复特殊方块放置bug

--- src/main/java/cn/zhuobing/testPlugin/anniPlayer/PlayerListener.java
+++ src/main/java/cn/zhuobing/testPlugin/anniPlayer/PlayerListener.java
@@ -1,16 +1,34 @@
 package cn.zhuobing.testPlugin.anniPlayer;
 
+import cn.zhuobing.testPlugin.ore.OreType;
 import cn.zhuobing.testPlugin.team.TeamManager;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class PlayerListener implements Listener {
     private final TeamManager teamManager;
+    private final Set<Material> prohibitedMaterials;
 
     public PlayerListener(TeamManager teamManager) {
         this.teamManager = teamManager;
+        // 初始化禁止放置的方块集合
+        this.prohibitedMaterials = new HashSet<>();
+        // 遍历 OreType 枚举
+        for (OreType oreType : OreType.values()) {
+            // 排除羊毛类和泥土类
+            if (oreType != OreType.WOOL && oreType != OreType.DIRT) {
+                // 将对应方块类型添加到禁止集合中
+                prohibitedMaterials.addAll(Arrays.asList(oreType.getSourceBlocks()));
+            }
+        }
     }
 
     @EventHandler
@@ -28,8 +46,19 @@ public class PlayerListener implements Listener {
             if (attackerTeamName != null && attackerTeamName.equals(victimTeamName)) {
                 // 阻止攻击行为
                 event.setCancelled(true);
-                attacker.sendMessage("你不能攻击自己的队友！");
             }
+        }
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+        Material placedMaterial = event.getBlock().getType();
+
+        // 检查放置的方块是否在禁止放置的方块集合中
+        if (prohibitedMaterials.contains(placedMaterial)) {
+            // 取消方块放置事件
+            event.setCancelled(true);
         }
     }
 }
